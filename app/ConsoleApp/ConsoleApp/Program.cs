a38399da95670a863b6f30895c755f911f8964ea
MaximUryukin <a54191781@gmail.com>
2025-12-26 02:25:01 +0300

    final commit

--- app/ConsoleApp/ConsoleApp/Program.cs
+++ app/ConsoleApp/ConsoleApp/Program.cs
@@ -2,9 +2,9 @@
 
 namespace app
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Enter first integer:");
             int a = int.Parse(Console.ReadLine());
@@ -16,7 +16,7 @@ namespace app
             Console.WriteLine($"Sum of {a} and {b} is {sum}.");
         }
 
-        static int Sum(int x, int y)
+        public static int Sum(int x, int y)
         {
             return x + y;
         }
---
+++ app/TestApp/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿    public class UnitTest1
+    {
+        [Fact]
+        public void Test_Sum_Of_Two_Numbers()
+        {
+            int actual = app.Program.Sum(3, 5);
+            Assert.Equal(8, actual);
+        }
+    }
