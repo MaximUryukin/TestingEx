9f372171d67f0d0ecbc66705234a4de50998b51c
MaximUryukin <a54191781@gmail.com>
2025-12-26 02:01:28 +0300

    first part

--- app/ConsoleApp/ConsoleApp/Program.cs
+++ app/ConsoleApp/ConsoleApp/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-//new comment
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first integer:");
+            int a = int.Parse(Console.ReadLine());
+
+        }
+
+        static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
