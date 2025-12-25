1023ae775a981749e3c6371b17eb4cbc28ddd768
MaximUryukin <a54191781@gmail.com>
2025-12-26 02:02:24 +0300

    third part

--- app/ConsoleApp/ConsoleApp/Program.cs
+++ app/ConsoleApp/ConsoleApp/Program.cs
@@ -12,6 +12,8 @@ namespace app
             Console.WriteLine("Enter second integer:");
             int b = int.Parse(Console.ReadLine());
 
+            int sum = Sum(a, b); // Функция вызывается здесь
+            Console.WriteLine($"Sum of {a} and {b} is {sum}.");
         }
 
         static int Sum(int x, int y)
